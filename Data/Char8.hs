fd5a3231e2c1b71883ce8e07a5e109db7eb4f023
Felix Paulusma <felix.paulusma@gmail.com>
2024-02-02 04:10:41 +0100

    added to and adjusted documentation of Data.Char8

--- Data/Char8.hs
+++ Data/Char8.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE MagicHash #-}
 
--- | Char8 library to be used with Data.ByteString.Char8.
--- All function assumes that only 8bit part of 'Char' is used
--- and it is encoded in Latin-1 (ISO-8859-1).
--- All utility functions are supposed to work as if
--- those of 'Data.Char'. Exceptions are described in
+-- | Char8 library to be used with "Data.ByteString.Char8".
+-- All functions assume that only the 8-bit part of 'Char'.
+-- All utility functions are supposed to work like
+-- those of "Data.Char". Exceptions are described in
 -- the function documentations.
 --
 -- Base library 4.7 (GHC 7.8) or earlier is based on Unicode 6.
+--
 -- Base library 4.8 (GHC 7.10) or later is based on Unicode 7.
+--
 -- 'isLower', 'isSymbol' and 'isPunctuation' behave differently.
 
 module Data.Char8 (
@@ -26,10 +27,12 @@ module Data.Char8 (
 import GHC.Base
 ----------------------------------------------------------------
 
+-- | Is it a control character
 isControl :: Char -> Bool
 isControl c = c <= '\x1f'
            || _del <= c && c <= '\x9f'
 
+-- | Is it white space
 isSpace :: Char -> Bool
 isSpace c = c == _space
          || c == _tab
@@ -39,8 +42,10 @@ isSpace c = c == _space
          || c == _vt
          || c == _nbsp
 
--- | This function returns 'True' for 170 and 186 in Unicode 6.
---   But it returns 'False' in Unicode 7.
+-- | Is it a lower case character
+--
+-- This function returns 'True' for 170 (@'ª'@) and 186 (@'º'@) in Unicode 6.
+-- But it returns 'False' in Unicode 7.
 isLower :: Char -> Bool
 isLower c = isLower' c
          || c == _mu
@@ -59,39 +64,55 @@ isLower' :: Char -> Bool
 isLower' c = isAsciiLower c
           || _germandbls <= c && c <= _ydieresis && c /= '\xf7'
 
+-- | Is it an upper case character
 isUpper :: Char -> Bool
 isUpper c = isAsciiUpper c
          || _Agrave <= c && c <=_Thorn && c /= '\xd7'
 
+-- | Is it an alphabet letter
+--
+-- Synonym for 'isLetter'
 isAlpha :: Char -> Bool
 isAlpha c = isLowerCommon c || isUpper c
 
+-- | Is it an alphabet letter or a number
 isAlphaNum :: Char -> Bool
 isAlphaNum c = isAlpha c || isNumber c
 
+-- | Is the character printable
 isPrint :: Char -> Bool
 isPrint c
   | c == _softhyphen = False
 isPrint c = _space <= c && c <= _tilde
          || _nbsp  <= c && c <= _ydieresis
 
+-- | Is the character a digit (@0-9@)
 isDigit :: Char -> Bool
 isDigit c = '0' <= c && c <= '9'
 
+-- | Is the character an octet digit (@0-7@)
 isOctDigit :: Char -> Bool
 isOctDigit c = '0' <= c && c <= '7'
 
+-- | Is the character a hexidecimal digit (@0-9a-zA-Z@)
 isHexDigit :: Char -> Bool
 isHexDigit c = isDigit c
             || 'A' <= c && c <= 'F'
             || 'a' <= c && c <= 'f'
 
+-- | Is it an alphabet letter
+--
+-- Synonym for 'isAlpha'
 isLetter :: Char -> Bool
 isLetter c = isLowerCommon c || isUpper c
 
+-- | A Latin-1 character is never a mark
+--
+-- Synonymous with @const False@
 isMark :: Char -> Bool
 isMark _ = False
 
+-- | Is the character a number
 isNumber :: Char -> Bool
 isNumber c = isDigit c
           || c == _s1
@@ -101,8 +122,10 @@ isNumber c = isDigit c
           || c == _1'2
           || c == _3'4
 
--- | This function returns 'False' for 167 and 182 in Unicode 6.
---   But it returns 'True' in Unicode 7.
+-- | Is it a punctuation character
+--
+-- This function returns 'False' for 167 (@'§'@) and 182 (@'¶'@) in Unicode 6.
+-- But it returns 'True' in Unicode 7.
 isPunctuation :: Char -> Bool
 isPunctuation c =
     c `elem`
@@ -116,8 +139,10 @@ isPunctuation c =
 #endif
         ,'\xa1','\xab','\xb7','\xbb','\xbf']
 
--- | This function returns 'True' for 167 and 182 in Unicode 6.
---   But it returns 'False' in Unicode 7.
+-- | Is it a symbol character
+--
+-- This function returns 'True' for 167 (@'§'@) and 182 (@'¶'@) in Unicode 6.
+-- But it returns 'False' in Unicode 7.
 isSymbol :: Char -> Bool
 isSymbol c =
     c `elem`
@@ -128,12 +153,14 @@ isSymbol c =
 #endif
         ,'\xa8','\xa9','\xac','\xae','\xaf','\xb0','\xb1','\xb4','\xb8','\xd7','\xf7']
 
+-- | Is it a (non-breaking) space
 isSeparator :: Char -> Bool
 isSeparator c = c == _space
              || c == _nbsp
 
 ----------------------------------------------------------------
 
+-- | Is the character in the ASCII range
 isAscii :: Char -> Bool
 #if __GLASGOW_HASKELL__ >= 707
 isAscii (C# c#) = isTrue# (ord# c# <=# 0x7f#)
@@ -141,6 +168,7 @@ isAscii (C# c#) = isTrue# (ord# c# <=# 0x7f#)
 isAscii (C# c#) = ord# c# <=# 0x7f#
 #endif
 
+-- | Is the character in the Latin-1 range
 isLatin1 :: Char -> Bool
 #if __GLASGOW_HASKELL__ >= 707
 isLatin1 (C# c#) = isTrue# (ord# c# <=# 0xff#)
@@ -148,27 +176,32 @@ isLatin1 (C# c#) = isTrue# (ord# c# <=# 0xff#)
 isLatin1 (C# c#) = ord# c# <=# 0xff#
 #endif
 
+-- | Is the character an upper case ASCII letter (@A-Z@)
 isAsciiUpper :: Char -> Bool
 isAsciiUpper c = 'A' <= c && c <= 'Z'
 
+-- | Is the character a lower case ASCII letter (@a-z@)
 isAsciiLower :: Char -> Bool
 isAsciiLower c = 'a' <= c && c <= 'z'
 
 ----------------------------------------------------------------
 
--- | Micro sign/mu (0xb5) and small letter Y with diaeresis (0xff) remain the same.
+-- | Changes the character to the upper case variant, if possible.
+--
+-- Micro sign/mu ('_mu') and small letter Y with diaeresis ('_ydieresis') remain the same.
 toUpper :: Char -> Char
 toUpper c@(C# c#)
   | c == _germandbls = c
   | isLower' c       = C# (chr# (ord# c# -# 32#))
   | otherwise        = c
 
+-- | Changes the character to the lower case variant, if possible.
 toLower :: Char -> Char
 toLower c@(C# c#)
   | isUpper c = C# (chr# (ord# c# +# 32#))
   | otherwise = c
 
--- | Micro sign/mu (0xb5) and small letter Y with diaeresis (0xff) remain the same.
+-- | Synonym for 'toUpper'
 toTitle :: Char -> Char
 toTitle = toUpper
 
